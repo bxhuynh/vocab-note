2155e22cf9d00802671f4cf5287ac12f9c5a9f0f
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2022-03-15 22:53:18 +0700

    create 3 fragments, add handler to navigation

--- app/src/main/java/me/bxhuynh/vocabnote/MainActivity.java
+++ app/src/main/java/me/bxhuynh/vocabnote/MainActivity.java
@@ -1,14 +1,47 @@
 package me.bxhuynh.vocabnote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    StudyingFragment studyingFragment = new StudyingFragment();
+    StatisticsFragment statisticsFragment = new StatisticsFragment();
+    AllVocabFragment allVocabFragment = new AllVocabFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id){
+                    case R.id.navigation_studying:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, studyingFragment).commit();
+                        return true;
+
+                    case R.id.navigation_all:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, allVocabFragment).commit();
+                        return true;
+
+                    case R.id.navigation_statistics:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, statisticsFragment).commit();
+                        return true;
+                }
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.navigation_studying);
     }
+
 }
---
+++ app/src/main/java/me/bxhuynh/vocabnote/StudyingFragment.java
@@ -0,0 +1,28 @@
+package me.bxhuynh.vocabnote;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StudyingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StudyingFragment extends Fragment {
+
+    public StudyingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_studying, container, false);
+    }
+}
